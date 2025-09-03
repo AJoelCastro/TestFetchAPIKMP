5777b6c614f3373b7fff4432218277aed94f21f4
AJoelCastro <misthorn33@gmail.com>
2025-09-03 11:46:10 -0500

    shared add dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -73,6 +73,9 @@ kotlin {
                 implementation(libs.androidx.lifecycle.runtimeCompose)
                 // Add KMP dependencies here
                 implementation(libs.kotlinx.serialization.json)
+                implementation(libs.coil.core)
+                implementation(libs.coil.network.ktor)
+                implementation(libs.ktor.client.core)
             }
         }
 
@@ -86,9 +89,7 @@ kotlin {
             dependencies {
                 implementation(compose.preview)
                 implementation(libs.androidx.activity.compose)
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
+                implementation(libs.ktor.client.okhttp)
             }
         }
 
@@ -100,14 +101,8 @@ kotlin {
             }
         }
 
-        iosMain {
-            dependencies {
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
-            }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
         }
     }
 
